a24ff872edb52db90af48d7bd2036d997d786052
Yu Feng <rainwoodman@gmail.com>
2019-01-04 15:41:49 -0800

    Add a master entry point for the compiler.

--- tinypy/compiler/__main__.py
+++ tinypy/compiler/__main__.py
@@ -1,5 +1,21 @@
 from .boot import *
-from .py2bc import main
+from . import py2bc
+
+def main(args=None):
+    if args is None: args = ARGV
+    if len(args) == 2:
+        src = args[1]
+        for i in range(len(args[1]) - 1, 0, -1):
+            if args[1][i] == '.': break
+
+        dest = args[1][:i] + '.tpc'
+    elif len(args) == 3:
+        src = args[1]
+        dest = args[2]
+    else:
+        print("Usage : tinypyc source.py [target.tpc]")
+        return
+    py2bc.main(src, dest)
 
 if __name__ == '__main__':
-    main(ARGV[1],ARGV[2])
+    main()
