47e723bef312aee6f982350daf4a3fb4dee420c8
Yu Feng <rainwoodman@gmail.com>
2019-02-17 10:29:20 -0800

    revert last change. The logic was wrong.

--- tinypy/tpd_dict.c
+++ tinypy/tpd_dict.c
@@ -30,7 +30,7 @@ void tpd_dict_hashset(TP, tpd_dict *self, int hash, tp_obj k, tp_obj v) {
 void tpd_dict_realloc(TP, tpd_dict *self, int len) {
     tpd_item *items = self->items;
     int i,alloc = self->alloc;
-    len = _tp_min(8,len);
+    len = _tp_max(8,len);
 
     self->items = (tpd_item*)tp_malloc(tp, len*sizeof(tpd_item));
     self->alloc = len; self->mask = len-1;
@@ -66,7 +66,9 @@ void tpd_dict_hashsetx(TP, tpd_dict * self, int hash, tp_obj k, tp_obj v) {
     if (n == -1) {
         if (self->len >= (self->alloc/2)) {
             tpd_dict_realloc(tp, self, self->alloc*2);
-        } else if (self->used >= (self->alloc*3/4)) {
+        } else
+        if (self->used >= (self->alloc /4 * 3)) {
+            /* compact the dict */
             tpd_dict_realloc(tp, self, self->alloc);
         }
         tpd_dict_hashset(tp, self, hash, k, v);
